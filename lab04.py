0ed3c55062fa2d09c9d8e64c2caf4a1c50b0628a
Claire Lee <jclairelee@gmail.com>
2025-01-31 10:33:55 -0800

    create lab week4

---
+++ lab04.py
@@ -0,0 +1,241 @@
+# Import the random library to use for the dice later
+import random
+
+# Hero's Attack Functions
+def hero_attacks(combat_strength, m_health_points):
+    ascii_image = """
+                                @@   @@ 
+                                @    @  
+                                @   @   
+               @@@@@@          @@  @    
+            @@       @@        @ @@     
+           @%         @     @@@ @       
+            @        @@     @@@@@     
+               @@@@@        @@       
+               @    @@@@                
+          @@@ @@                        
+       @@     @                         
+   @@*       @                          
+   @        @@                          
+           @@                                                    
+         @   @@@@@@@                    
+        @            @                  
+      @              @                  
+      """
+    print(ascii_image)
+    print("Player's weapon (" + str(combat_strength) + ") ---> Monster (" + str(m_health_points) + ")")
+    if combat_strength >= m_health_points:
+        m_health_points = 0
+        print("You have killed the monster")
+    else:
+        m_health_points -= combat_strength
+        print("You have reduced the monster's health to " + str(m_health_points))
+    return m_health_points
+
+# Q1: Monster's Strength Value
+monster_strength ={
+    "Fire Magic":  2,
+    "Freeze Time":  4,
+    "Super Hearing": 6
+}
+print(monster_strength["Fire Magic"])
+# Monster's Attack Function
+
+def monster_attacks(m_combat_strength, health_points):
+    ascii_image2 = """                                                                 
+           @@@@ @                           
+      (     @*&@  ,                         
+    @               %                       
+     &#(@(@%@@@@@*   /                      
+      @@@@@.                                
+               @       /                    
+                %         @                 
+            ,(@(*/           %              
+               @ (  .@#                 @   
+                          @           .@@. @
+                   @         ,              
+                      @       @ .@          
+                             @              
+                          *(*  *      
+             """
+    print(ascii_image2)
+    print("Monster's Claw (" + str(m_combat_strength) + ") ---> Hero (" + str(health_points) + ")")
+    if m_combat_strength >= health_points:
+        health_points = 0
+        print("You have killed the monster")
+    else:
+        health_points -= m_combat_strength
+        print("The monster has reduced your health to " + str(health_points))
+    return health_points
+
+
+# Game
+# Define The number of lives for the Hero and Monster
+numLives = 10  # number of player's lives remaining
+mNumLives = 12  # number of monster's lives remaining
+
+# Define the Dice
+diceOptions = list(range(1, 7))
+# Define the Weapons
+weapons = ["Fist", "Knife", "Club", "Gun", "Bomb", "Nuclear Bomb"]
+
+# Print out the weapons using a for loop
+for weapon in weapons:
+    print(weapon)
+
+# Define the Loot
+loot_options = ["Health Potion", "Poison Potion", "Secret Note", "Leather Boots", "Flimsy Gloves"]
+good_loot_options = ["Health Potion", "Leather Boots"]
+bad_loot_options = ["Poison Potion"]
+
+# Define the number of stars awarded to the Player
+num_stars = 0
+
+# Use a While Loop to get valid input for Hero and Monster's Combat Strength
+i = 0
+
+while i in range(5):
+    combat_strength = input("Enter your combat Strength (1-6): ")
+    m_combat_strength = input("Enter the monster's combat Strength (1-6): ")
+
+    # Validate input: Check if the string inputted is numeric
+    if (not combat_strength.isnumeric()) or (not m_combat_strength.isnumeric()):
+        # If one of the inputs are invalid, print error message and halt
+        print("One or more invalid inputs. Player needs to enter integer numbers for Combat Strength")
+        i = i + 1
+        continue
+
+    # Note: Now safe to cast combat_strength to integer
+    # Validate input: Check if the string inputted is a number between 1-6
+    elif (int(combat_strength) not in range(1, 7)) or (int(m_combat_strength)) not in range(1, 7):
+        print("Enter a valid integer between 1 and 6 only")
+        i = i + 1
+        continue
+
+    # Break out of while loop if input was valid
+    else:
+        break
+
+# Input was valid - broke out of while loop
+combat_strength = int(combat_strength)
+m_combat_strength = int(m_combat_strength)
+
+# Roll for weapon
+input("Roll the dice for your weapon (Press enter)")
+weaponRoll = random.choice(diceOptions)
+
+# Q2: Roll for Monster's Magic Power
+input("Roll for the monster's magic power (Press enter)")
+monster_power_keys = list(monster_strength.keys())
+monster_power = random.choice(monster_power_keys)
+m_combat_strength = min(6, monster_strength[monster_power])
+print(f"Monster used {monster_power} and now has combat strength {m_combat_strength}")
+
+# Q3: Update Monster's Combat Strength
+m_combat_strength = min(6, m_combat_strength + monster_strength[monster_power])
+
+if m_combat_strength > 6:
+    m_combat_strength = 6
+
+print(f"Monster got{monster_power}, increasing their combat stenghth to :{m_combat_strength}")
+min(5, 3)
+
+# Q4. Define an new empty array called belt
+belt = []
+
+# Max out the combat strength at 6
+combat_strength = min(6, (combat_strength + weaponRoll))
+print("The hero\'s weapon is " + str(weapons[weaponRoll - 1]))
+
+# Weapon Roll Analysis
+input("Analyze the Weapon roll (Press enter)")
+if weaponRoll <= 2:
+    print("--- You rolled a weak weapon, friend")
+elif weaponRoll <= 4:
+    print("--- Your weapon is meh")
+else:
+    print("--- Nice weapon, friend!")
+
+# If the weapon rolled is not a Fist, print out "Thank goodness you didn't roll the Fist..."
+if weapons[weaponRoll - 1] != "Fist":
+    print("--- Thank goodness you didn't roll the Fist...")
+
+# Roll for player health points
+input("Roll the dice for your health points (Press enter)")
+health_points = random.choice(diceOptions)
+print("Player rolled " + str(health_points) + " health points")
+
+# Roll for monster health points
+input("Roll the dice for the monster's health points (Press enter)")
+m_health_points = random.choice(diceOptions)
+print("Player rolled " + str(m_health_points) + " health points for the monster")
+
+
+# Q5: The player collects loot
+print("You found loot!")
+input("Roll the dice to find your first item (Press enter)")
+# loot_item1 = random.choice(loot_options)
+# loot_options.pop()
+loot_item1_idx = random.choice(list(range(0, 5)))
+loot_item1=loot_options.pop(loot_item1_idx)
+belt.append(loot_item1)
+print(f"You now have {belt}")
+
+#Q6: The player coolects more loot
+print("You found more loot!")
+input("Roll the dice to find your second item (Press enter)")
+loot_item2_idx = random.choice(list(range(0, 5)))
+loot_item2=loot_options.pop(loot_item2_idx)
+belt.append(loot_item2)
+print(f"You now have {belt}")
+
+#Q7. Organizing the loot Belt
+belt.sort()
+print(belt)
+
+# Q8: Use first item from the belt
+# Remove and retrieve the first item from the player's belt
+use_item = belt.pop()
+
+# Check if the item is in the good loot options (beneficial items)
+if use_item in good_loot_options:
+    # Increase health points but ensure it does not exceed 6
+    health_points = min(6, health_points + 2)
+    print(f"You used {use_item}, and your health increased to {health_points}")
+
+# Check if the item is in the bad loot options (harmful items)
+elif use_item in bad_loot_options:
+    # Decrease health points but ensure it does not go below 0
+    health_points = max(0, health_points - 2)
+    print(f"Oh no! You used {use_item}, and your health dropped to {health_points}")
+
+# If the item is neither good nor bad, it has no effect
+else:
+    print(f"You used {use_item}, but it had no effect.")
+
+
+input("Analyze the roll (Press enter)")
+# Compare Player vs Monster's strength
+print("--- You are matched in strength: " + str(combat_strength == m_combat_strength))
+
+# Check the Player's overall strength and health
+print("--- You have a strong player: " + str((combat_strength + health_points) >= 15))
+
+# Loop while the monster and the player are alive. Call fight sequence functions
+print("You meet the monster. FIGHT!!")
+while m_health_points > 0 and health_points > 0:
+
+    input("You strike first (Press Enter)")
+    m_health_points = hero_attacks(combat_strength, m_health_points)
+    if m_health_points == 0:
+        num_stars = 3
+    else:
+        input("The monster strikes (Press Enter)")
+        health_points = monster_attacks(m_combat_strength, health_points)
+        if health_points == 0:
+            num_stars = 1
+        else:
+            num_stars = 2
+
+stars = "*" * num_stars
+print("Hero gets <" + stars + "> stars")
